74528dad642763761e549924fe37343c4322adb9
Uraneptus <uraneptus33@gmail.com>
2022-06-14 14:31:37 +0200

    Grindstone particles + no shift when polishing

--- src/main/java/com/uraneptus/sullysmod/core/events/SMPlayerEvents.java
+++ src/main/java/com/uraneptus/sullysmod/core/events/SMPlayerEvents.java
@@ -1,16 +1,23 @@
 package com.uraneptus.sullysmod.core.events;
 
+import com.mojang.math.Vector3f;
 import com.uraneptus.sullysmod.SullysMod;
 import com.uraneptus.sullysmod.core.registry.SMItems;
 import com.uraneptus.sullysmod.core.registry.SMSounds;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.DustParticleOptions;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.ParticleUtils;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.GrindstoneBlock;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -27,22 +34,42 @@ public class SMPlayerEvents {
         InteractionHand hand = event.getHand();
         Block block = level.getBlockState(pos).getBlock();
 
-        if (player.isShiftKeyDown()) {
-            if (block instanceof GrindstoneBlock) {
-                if (player.getItemInHand(hand).is(SMItems.RAW_JADE.get())) {
-                    if (!player.getAbilities().instabuild) {
-                        item.shrink(1);
-                    }
+        if (block instanceof GrindstoneBlock) {
+            if (player.getItemInHand(hand).is(SMItems.RAW_JADE.get())) {
+                event.setCanceled(true);
+                event.setCancellationResult(InteractionResult.FAIL);
 
-                    if (item.isEmpty()) {
-                        player.setItemInHand(hand, new ItemStack(SMItems.JADE.get(), 1));
-                    } else {
-                        ItemStack itemStack = new ItemStack(SMItems.JADE.get(), 1);
-                        if (!player.getInventory().add(itemStack)) {
-                            player.drop(itemStack, false);
-                        }
+                if (!player.getAbilities().instabuild) {
+                    item.shrink(1);
+                }
+
+                if (item.isEmpty()) {
+                    player.setItemInHand(hand, new ItemStack(SMItems.JADE.get(), 1));
+                } else {
+                    ItemStack itemStack = new ItemStack(SMItems.JADE.get(), 1);
+                    if (!player.getInventory().add(itemStack)) {
+                        player.drop(itemStack, false);
                     }
-                    level.playSound(player, pos, SMSounds.POLISH_JADE.get(), SoundSource.BLOCKS, 0.5F, 0.0F);
+                }
+                player.swing(hand);
+                level.playSound(player, pos, SMSounds.POLISH_JADE.get(), SoundSource.BLOCKS, 0.5F, 0.0F);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        Player player = event.player;
+        Level level = player.getLevel();
+        InteractionHand hand = player.getUsedItemHand();
+        BlockPos pos = player.blockPosition();
+        Vec3 vec3 = player.position();
+
+        if (player.getItemInHand(hand).is(SMItems.RAW_JADE.get())) {
+            for(BlockPos blockpos : BlockPos.betweenClosed(pos.offset(-5, -5, -5), pos.offset(5, 5, 5))) {
+                Block block = level.getBlockState(blockpos).getBlock();
+                if (block instanceof GrindstoneBlock) {
+                    ParticleUtils.spawnParticlesOnBlockFaces(level, blockpos, new DustParticleOptions(new Vector3f(Vec3.fromRGB24(16777215)), 0.4F), UniformInt.of(0, 1));
                 }
             }
         }
