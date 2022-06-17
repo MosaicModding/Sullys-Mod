e51784f1570a52d68b7c7a36e7ae97fb297bea75
Mana Kampman <manastar04@gmail.com>
2022-06-17 07:48:56 -0700

    Closes #13

--- src/main/java/com/uraneptus/sullysmod/client/renderer/entities/layer/LanternfishGlowLayer.java
+++ src/main/java/com/uraneptus/sullysmod/client/renderer/entities/layer/LanternfishGlowLayer.java
@@ -2,6 +2,7 @@ package com.uraneptus.sullysmod.client.renderer.entities.layer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.teamabnormals.blueprint.client.BlueprintRenderTypes;
 import com.uraneptus.sullysmod.SullysMod;
 import com.uraneptus.sullysmod.client.renderer.entities.model.LanternfishModel;
 import com.uraneptus.sullysmod.common.entities.LanternfishEntity;
@@ -14,7 +15,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 
 public class LanternfishGlowLayer extends RenderLayer<LanternfishEntity, LanternfishModel<LanternfishEntity>> {
-    public static final ResourceLocation TEXTURE = new ResourceLocation(SullysMod.MOD_ID, "textures/entity/lanternfish/glow.png");
+    public static final ResourceLocation GLOW_LAYER = new ResourceLocation(SullysMod.MOD_ID, "textures/entity/lanternfish/glow.png");
 
     public LanternfishGlowLayer(RenderLayerParent<LanternfishEntity, LanternfishModel<LanternfishEntity>> entityRenderer) {
         super(entityRenderer);
@@ -22,11 +23,10 @@ public class LanternfishGlowLayer extends RenderLayer<LanternfishEntity, Lantern
 
     @Override
     public void render(PoseStack pMatrixStack, MultiBufferSource pBuffer, int pPackedLight, LanternfishEntity pLivingEntity, float pLimbSwing, float pLimbSwingAmount, float pPartialTicks, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch) {
-        int i = (int)Mth.clampedLerp(0.0F, 15.0F, 1.0F - (float) pLivingEntity.getDarkTicksRemaining() / 10.0F);
-        if (i == 15) {
-            VertexConsumer vertexconsumer = pBuffer.getBuffer(RenderType.eyes(TEXTURE));
-            this.getParentModel().renderToBuffer(pMatrixStack, vertexconsumer, pPackedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
-        }
-
+        int i = (int) Mth.clampedLerp(0.0F, 15.0F, 1.0F - (float) pLivingEntity.getDarkTicksRemaining() / 10.0F);
+        int packedLight = i == 15 ? 240 : pPackedLight;
+        RenderType renderType = i == 15 ? BlueprintRenderTypes.getUnshadedCutoutEntity(GLOW_LAYER, false) : RenderType.entityCutoutNoCull(GLOW_LAYER);
+        VertexConsumer vertexconsumer = pBuffer.getBuffer(renderType);
+        this.getParentModel().renderToBuffer(pMatrixStack, vertexconsumer, packedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
