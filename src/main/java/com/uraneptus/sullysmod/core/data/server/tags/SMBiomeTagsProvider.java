3180f1bbcae1e6c50e4b87e6862198bd87d211bd
Uraneptus <uraneptus33@gmail.com>
2022-07-24 14:30:05 +0200

    fixed lanternfish_spawn_in tag

--- src/main/java/com/uraneptus/sullysmod/core/data/server/tags/SMBiomeTagsProvider.java
+++ src/main/java/com/uraneptus/sullysmod/core/data/server/tags/SMBiomeTagsProvider.java
@@ -7,7 +7,9 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BiomeTagsProvider;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biomes;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.common.data.ForgeBiomeTagsProvider;
 import org.jetbrains.annotations.Nullable;
 
 public class SMBiomeTagsProvider extends BiomeTagsProvider {
@@ -23,6 +25,22 @@ public class SMBiomeTagsProvider extends BiomeTagsProvider {
                 Biomes.BIRCH_FOREST,
                 Biomes.OLD_GROWTH_BIRCH_FOREST
         );
-        tag(SMBiomeTags.LANTERNFISH_SPAWN_IN).addTag(BlueprintBiomeTags.IS_OVERWORLD);
+        tag(SMBiomeTags.LANTERNFISH_SPAWN_IN)
+                .addTag(BiomeTags.IS_OCEAN)
+                .addTag(BiomeTags.IS_FOREST)
+                .addTag(BiomeTags.IS_SAVANNA)
+                .addTag(BiomeTags.IS_MOUNTAIN)
+                .addTag(BiomeTags.IS_BADLANDS)
+                .addTag(BiomeTags.IS_JUNGLE)
+                .addTag(BiomeTags.IS_BEACH)
+                .addTag(BiomeTags.IS_RIVER)
+                .addTag(BiomeTags.IS_HILL)
+                .addTag(BiomeTags.IS_TAIGA)
+                .addTag(Tags.Biomes.IS_SNOWY)
+                .addTag(Tags.Biomes.IS_PLAINS)
+                .addTag(Tags.Biomes.IS_SANDY)
+                .addTag(Tags.Biomes.IS_SWAMP)
+                .addTag(Tags.Biomes.IS_MUSHROOM)
+                .add(Biomes.DRIPSTONE_CAVES);
     }
 }
