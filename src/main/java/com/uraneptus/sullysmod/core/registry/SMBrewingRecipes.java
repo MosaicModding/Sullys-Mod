292106406b52dfb98ae4227f6938a69bff69940d
gohkenytp <34868610+gohkenytp@users.noreply.github.com>
2022-08-26 17:45:07 -0400

    Update SMBrewingRecipes.java

--- src/main/java/com/uraneptus/sullysmod/core/registry/SMBrewingRecipes.java
+++ src/main/java/com/uraneptus/sullysmod/core/registry/SMBrewingRecipes.java
@@ -9,5 +9,6 @@ public class SMBrewingRecipes {
     public static void register() {
         DataUtil.addMix(Potions.AWKWARD, SMItems.POLISHED_JADE.get(), Potions.LUCK);
         DataUtil.addMix(Potions.LUCK, Items.FERMENTED_SPIDER_EYE, SMPotions.UNLUCK.get());
+        DataUtil.addMix(Potions.SLOWNESS, Items.COAL_BLOCK, SMPotions.BLINDNESS.get());
     }
 }
