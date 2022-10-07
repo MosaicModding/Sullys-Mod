66d9598588a86efb3a0555201e9f66bd09593fcd
Uraneptus <uraneptus33@gmail.com>
2022-10-07 17:07:07 +0200

    Update SMSoundDefinitionsProvider.java

--- src/main/java/com/uraneptus/sullysmod/core/data/client/SMSoundDefinitionsProvider.java
+++ src/main/java/com/uraneptus/sullysmod/core/data/client/SMSoundDefinitionsProvider.java
@@ -2,9 +2,13 @@ package com.uraneptus.sullysmod.core.data.client;
 
 import com.uraneptus.sullysmod.SullysMod;
 import com.uraneptus.sullysmod.core.registry.SMSounds;
+import net.minecraft.client.resources.sounds.Sound;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.common.data.SoundDefinition;
 import net.minecraftforge.common.data.SoundDefinitionsProvider;
@@ -25,35 +29,35 @@ public class SMSoundDefinitionsProvider extends SoundDefinitionsProvider {
         addMusicDiscSound(SMSounds.MUSIC_DISC_SCOUR, "scour");
 
         //Block Sounds
-        addBlockSound(SMSounds.POLISH_JADE,
+        addbasicSound(SMSounds.POLISH_JADE,
                 sound(SullysMod.modPrefix("block/grindstone/jade_polish0")),
                 sound(SullysMod.modPrefix("block/grindstone/jade_polish1"))
         );
 
-        addBlockSound(SMSounds.JADE_RICOCHET,
+        addbasicSound(SMSounds.JADE_RICOCHET,
                 sound(new ResourceLocation("block/end_portal/eyeplace1")),
                 sound(new ResourceLocation("block/end_portal/eyeplace2")),
                 sound(new ResourceLocation("block/end_portal/eyeplace3"))
         );
 
-        addBlockSound(SMSounds.FLINGER_FLINGS,
+        addbasicSound(SMSounds.FLINGER_FLINGS,
                 sound(new ResourceLocation("random/bow"))
         );
 
         //Entity Sounds
-        addEntitySound(SMSounds.TORTOISE_AMBIENT,
+        addbasicSound(SMSounds.TORTOISE_AMBIENT,
                 sound(new ResourceLocation("mob/turtle/idle1")).volume(0.8F),
                 sound(new ResourceLocation("mob/turtle/idle2")).volume(0.7F),
                 sound(new ResourceLocation("mob/turtle/idle3")).volume(0.8F)
         );
 
-        addEntitySound(SMSounds.TORTOISE_DEATH,
+        addbasicSound(SMSounds.TORTOISE_DEATH,
                 sound(new ResourceLocation("mob/turtle/death1")),
                 sound(new ResourceLocation("mob/turtle/death2")),
                 sound(new ResourceLocation("mob/turtle/death3"))
         );
 
-        addEntitySound(SMSounds.TORTOISE_HURT,
+        addbasicSound(SMSounds.TORTOISE_HURT,
                 sound(new ResourceLocation("mob/turtle/hurt1")),
                 sound(new ResourceLocation("mob/turtle/hurt2")),
                 sound(new ResourceLocation("mob/turtle/hurt3")),
@@ -61,7 +65,7 @@ public class SMSoundDefinitionsProvider extends SoundDefinitionsProvider {
                 sound(new ResourceLocation("mob/turtle/hurt5"))
         );
 
-        addEntitySound(SMSounds.TORTOISE_HIDE,
+        addbasicSound(SMSounds.TORTOISE_HIDE,
                 sound(new ResourceLocation("entity/shulker/close1")),
                 sound(new ResourceLocation("entity/shulker/close2")),
                 sound(new ResourceLocation("entity/shulker/close3")),
@@ -69,7 +73,7 @@ public class SMSoundDefinitionsProvider extends SoundDefinitionsProvider {
                 sound(new ResourceLocation("entity/shulker/close5"))
         );
 
-        addEntitySound(SMSounds.TORTOISE_EMERGE,
+        addbasicSound(SMSounds.TORTOISE_EMERGE,
                 sound(new ResourceLocation("entity/shulker/open1")),
                 sound(new ResourceLocation("entity/shulker/open2")),
                 sound(new ResourceLocation("entity/shulker/open3")),
@@ -77,7 +81,7 @@ public class SMSoundDefinitionsProvider extends SoundDefinitionsProvider {
                 sound(new ResourceLocation("entity/shulker/open5"))
         );
 
-        addEntitySound(SMSounds.TORTOISE_HURT_HIDDEN,
+        addbasicSound(SMSounds.TORTOISE_HURT_HIDDEN,
                 sound(new ResourceLocation("entity/shulker/hurt_closed1")),
                 sound(new ResourceLocation("entity/shulker/hurt_closed2")),
                 sound(new ResourceLocation("entity/shulker/hurt_closed3")),
@@ -85,27 +89,27 @@ public class SMSoundDefinitionsProvider extends SoundDefinitionsProvider {
                 sound(new ResourceLocation("entity/shulker/hurt_closed5"))
         );
 
-        addEntitySound(SMSounds.BABY_TORTOISE_HURT,
+        addbasicSound(SMSounds.BABY_TORTOISE_HURT,
                 sound(new ResourceLocation("mob/turtle/baby/hurt1")),
                 sound(new ResourceLocation("mob/turtle/baby/hurt2"))
         );
 
-        addEntitySound(SMSounds.BABY_TORTOISE_DEATH,
+        addbasicSound(SMSounds.BABY_TORTOISE_DEATH,
                 sound(new ResourceLocation("mob/turtle/baby/death1")),
                 sound(new ResourceLocation("mob/turtle/baby/death2"))
         );
 
-        addEntitySound(SMSounds.TORTOISE_LAY_EGG,
+        addbasicSound(SMSounds.TORTOISE_LAY_EGG,
                 sound(new ResourceLocation("mob/turtle/egg/drop_egg1")),
                 sound(new ResourceLocation("mob/turtle/egg/drop_egg2"))
         );
 
-        addEntitySound(SMSounds.TORTOISE_EGG_BREAK,
+        addbasicSound(SMSounds.TORTOISE_EGG_BREAK,
                 sound(new ResourceLocation("mob/turtle/egg/egg_break1")),
                 sound(new ResourceLocation("mob/turtle/egg/egg_break2"))
         );
 
-        addEntitySound(SMSounds.TORTOISE_EGG_CRACK,
+        addbasicSound(SMSounds.TORTOISE_EGG_CRACK,
                 sound(new ResourceLocation("mob/turtle/egg/egg_crack1")),
                 sound(new ResourceLocation("mob/turtle/egg/egg_crack2")),
                 sound(new ResourceLocation("mob/turtle/egg/egg_crack3")),
@@ -113,27 +117,27 @@ public class SMSoundDefinitionsProvider extends SoundDefinitionsProvider {
                 sound(new ResourceLocation("mob/turtle/egg/egg_crack5"))
         );
 
-        addEntitySound(SMSounds.TORTOISE_EGG_HATCH,
+        addbasicSound(SMSounds.TORTOISE_EGG_HATCH,
                 sound(new ResourceLocation("mob/turtle/baby/egg_hatched1")),
                 sound(new ResourceLocation("mob/turtle/baby/egg_hatched2")),
                 sound(new ResourceLocation("mob/turtle/baby/egg_hatched3"))
         );
 
-        addEntitySound(SMSounds.LANTERNFISH_FLOP,
+        addbasicSound(SMSounds.LANTERNFISH_FLOP,
                 sound(new ResourceLocation("entity/fish/flop1")).volume(0.3),
                 sound(new ResourceLocation("entity/fish/flop2")).volume(0.3),
                 sound(new ResourceLocation("entity/fish/flop3")).volume(0.3),
                 sound(new ResourceLocation("entity/fish/flop4")).volume(0.3)
         );
 
-        addEntitySound(SMSounds.LANTERNFISH_DEATH,
+        addbasicSound(SMSounds.LANTERNFISH_DEATH,
                 sound(new ResourceLocation("entity/fish/hurt1")),
                 sound(new ResourceLocation("entity/fish/hurt2")),
                 sound(new ResourceLocation("entity/fish/hurt3")),
                 sound(new ResourceLocation("entity/fish/hurt4"))
         );
 
-        addEntitySound(SMSounds.LANTERNFISH_HURT,
+        addbasicSound(SMSounds.LANTERNFISH_HURT,
                 sound(new ResourceLocation("entity/fish/hurt1")),
                 sound(new ResourceLocation("entity/fish/hurt2")),
                 sound(new ResourceLocation("entity/fish/hurt3")),
@@ -141,11 +145,7 @@ public class SMSoundDefinitionsProvider extends SoundDefinitionsProvider {
         );
     }
 
-    private void addBlockSound(Supplier<SoundEvent> soundEvent, SoundDefinition.Sound... sounds) {
-        this.add(soundEvent.get(), SoundDefinition.definition().subtitle("subtitles." + soundEvent.get().getLocation().getPath()).with(sounds));
-    }
-
-    private void addEntitySound(Supplier<SoundEvent> soundEvent, SoundDefinition.Sound... sounds) {
+    private void addbasicSound(Supplier<SoundEvent> soundEvent, SoundDefinition.Sound... sounds) {
         this.add(soundEvent.get(), SoundDefinition.definition().subtitle("subtitles." + soundEvent.get().getLocation().getPath()).with(sounds));
     }
 
