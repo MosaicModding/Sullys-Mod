01b13d8d1b532d1aecb6dde9230abf72ad1b6f25
Uraneptus <50745606+Uraneptus@users.noreply.github.com>
2022-12-31 17:58:36 +0100

    some really messy rascal changes

--- src/main/java/com/uraneptus/sullysmod/client/renderer/entities/RascalRenderer.java
+++ src/main/java/com/uraneptus/sullysmod/client/renderer/entities/RascalRenderer.java
@@ -1,14 +1,30 @@
 package com.uraneptus.sullysmod.client.renderer.entities;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.uraneptus.sullysmod.client.model.RascalModel;
 import com.uraneptus.sullysmod.common.entities.Rascal;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.util.Mth;
+import software.bernie.geckolib3.core.util.Color;
+import software.bernie.geckolib3.geo.render.built.GeoModel;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+import javax.annotation.Nullable;
 
 public class RascalRenderer <E extends Rascal> extends GeoEntityRenderer<E> {
 
     public RascalRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new RascalModel<>());
         this.shadowRadius = 0.5F;
+    }
+
+    @Override
+    public Color getRenderColor(E animatable, float partialTick, PoseStack poseStack, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, int packedLight) {
+        Color color = super.getRenderColor(animatable, partialTick, poseStack, bufferSource, buffer, packedLight);
+        return Color.ofRGBA(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, Mth.lerp(partialTick, animatable.getOldAlphaTick(), animatable.getAlphaTick() / 80f) / 255f);
     }
 }
--- src/main/java/com/uraneptus/sullysmod/common/entities/Rascal.java
+++ src/main/java/com/uraneptus/sullysmod/common/entities/Rascal.java
@@ -19,6 +19,8 @@ import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
 import net.minecraft.world.phys.Vec3;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -42,8 +44,12 @@ public class Rascal extends PathfinderMob implements IAnimatable {
 
     private static final EntityDataAccessor<Integer> TIMES_LOOKED_AT = SynchedEntityData.defineId(Rascal.class, EntityDataSerializers.INT);
     private static final EntityDataAccessor<Integer> LOOKED_AT_COOLDOWN = SynchedEntityData.defineId(Rascal.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Float> ALPHA_TICK = SynchedEntityData.defineId(Rascal.class, EntityDataSerializers.FLOAT);
+    private static final EntityDataAccessor<Float> OLD_ALPHA_TICK = SynchedEntityData.defineId(Rascal.class, EntityDataSerializers.FLOAT);
     int timesLookedAt;
     int lookedAtCooldown;
+    public float alphaTick = 80.0f;
+    public float alphaTickOld;
 
     @Nullable
     private UUID trackingTarget;
@@ -58,6 +64,7 @@ public class Rascal extends PathfinderMob implements IAnimatable {
     public Rascal(EntityType<? extends PathfinderMob> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
         this.setPathfindingMalus(BlockPathTypes.LAVA, -1.0F);
+        //alphaTickOld = alphaTick;
     }
 
     protected void registerGoals() {
@@ -80,6 +87,8 @@ public class Rascal extends PathfinderMob implements IAnimatable {
         super.defineSynchedData();
         this.entityData.define(TIMES_LOOKED_AT, timesLookedAt);
         this.entityData.define(LOOKED_AT_COOLDOWN, lookedAtCooldown);
+        this.entityData.define(ALPHA_TICK, alphaTick);
+        this.entityData.define(OLD_ALPHA_TICK, alphaTickOld);
         this.entityData.define(DATA_FLAGS_ID, (byte) 0);
 
     }
@@ -88,6 +97,8 @@ public class Rascal extends PathfinderMob implements IAnimatable {
         super.addAdditionalSaveData(nbt);
         nbt.putInt("TimesLookedAt", getTimesLookedAt());
         nbt.putInt("LookedAtCooldown", getLookedAtCooldown());
+        nbt.putFloat("AlphaTick", getAlphaTick());
+        nbt.putFloat("OldAlphaTick", getOldAlphaTick());
 
         nbt.putBoolean("LookedAt", this.isLookedAt());
         nbt.putBoolean("Fading", this.isFading());
@@ -98,6 +109,8 @@ public class Rascal extends PathfinderMob implements IAnimatable {
         super.readAdditionalSaveData(nbt);
         this.setTimesLookedAt(nbt.getInt("TimesLookedAt"));
         this.setLookedAtCooldown(nbt.getInt("LookedAtCooldown"));
+        this.setAlphaTick(nbt.getFloat("AlphaTick"));
+        this.setOldAlphaTick(nbt.getFloat("OldAlphaTick"));
 
         this.setLookedAt(nbt.getBoolean("LookedAt"));
         this.setFading(nbt.getBoolean("Fading"));
@@ -106,7 +119,9 @@ public class Rascal extends PathfinderMob implements IAnimatable {
 
     public void onSyncedDataUpdated(EntityDataAccessor<?> pKey) {
         super.onSyncedDataUpdated(pKey);
-
+        lookedAtCooldown = getLookedAtCooldown();
+        alphaTickOld = getOldAlphaTick();
+        alphaTick = getAlphaTick();
     }
 
     public boolean isLookingAtMe() {
@@ -122,6 +137,10 @@ public class Rascal extends PathfinderMob implements IAnimatable {
         return false;
     }
 
+    public float getWalkTargetValue(BlockPos pPos, LevelReader pLevel) {
+        return pLevel.getBrightness(LightLayer.BLOCK, this.blockPosition()) == 0 ? 10.0F : super.getWalkTargetValue(pPos, pLevel);
+    }
+
 
     public void aiStep() {
         super.aiStep();
@@ -132,6 +151,9 @@ public class Rascal extends PathfinderMob implements IAnimatable {
         SullysMod.LOGGER.info("Times looked at {}", getTimesLookedAt());
         SullysMod.LOGGER.info("Has been looked at {}", isLookedAt());
         System.out.println("Cooldown: " + getLookedAtCooldown());
+        System.out.println("Old Alpha tick: " + getOldAlphaTick());
+        System.out.println("Alpha tick: " + getAlphaTick());
+
 
 
 
@@ -142,22 +164,44 @@ public class Rascal extends PathfinderMob implements IAnimatable {
     public void tick() {
         super.tick();
 
+
         if (!this.level.isClientSide()) {
             if (this.getLookedAtCooldown() > 0) {
                 this.entityData.set(LOOKED_AT_COOLDOWN, this.getLookedAtCooldown() - 1);
+                this.setOldAlphaTick(getAlphaTick());
+                this.entityData.set(ALPHA_TICK, getAlphaTick() - 1);
+            }
+            if (getAlphaTick() > 0) {
+
 
             }
 
-            if (this.getLookedAtCooldown() == 50) {
-                this.setLookedAt(false);
+            if (this.getLookedAtCooldown() == 70) {
+                setLookedAt(false);
+
             }
 
-            if (this.getLookedAtCooldown() == 0 && this.isLookingAtMe() && this.getTimesLookedAt() < 3) {
-                this.entityData.set(TIMES_LOOKED_AT, this.getTimesLookedAt() + 1);
-                this.setLookedAtCooldown(60);
-                this.setLookedAt(true);
-                this.setTarget(player);
+            if (this.getLookedAtCooldown() == 0) {
+
+                if (this.isLookingAtMe() && this.getTimesLookedAt() < 3) {
+                    this.entityData.set(TIMES_LOOKED_AT, this.getTimesLookedAt() + 1);
+                    this.setLookedAtCooldown(80);
+                    this.setLookedAt(true);
+                    this.setFading(true);
+                    this.setTarget(player);
+                }
+                setAlphaTick(80.0f);
+                setOldAlphaTick(0);
+
             }
+            if (getAlphaTick() == 0) {
+
+            }
+
+            if (this.getLookedAtCooldown() != 0) {
+
+            }
+
 
         }
     }
@@ -203,6 +247,22 @@ public class Rascal extends PathfinderMob implements IAnimatable {
 
     public void setLookedAtCooldown(int amount) {
         this.entityData.set(LOOKED_AT_COOLDOWN, amount);
+    }
+
+    public float getAlphaTick() {
+        return this.entityData.get(ALPHA_TICK);
+    }
+
+    public void setAlphaTick(float amount) {
+        this.entityData.set(ALPHA_TICK, amount);
+    }
+
+    public float getOldAlphaTick() {
+        return this.entityData.get(OLD_ALPHA_TICK);
+    }
+
+    public void setOldAlphaTick(float amount) {
+        this.entityData.set(OLD_ALPHA_TICK, amount);
     }
 
     public boolean isLookedAt() {
@@ -284,13 +344,13 @@ public class Rascal extends PathfinderMob implements IAnimatable {
 
     //WTF is happening here?!
     static class RascalLookForDarkPlaceToFade extends Goal {
-        private final PathfinderMob mob;
+        private final Rascal mob;
         private double wantedX;
         private double wantedY;
         private double wantedZ;
         private final double speedModifier;
 
-        public RascalLookForDarkPlaceToFade(PathfinderMob mob, double pSpeedModifier) {
+        public RascalLookForDarkPlaceToFade(Rascal mob, double pSpeedModifier) {
             this.mob = mob;
             this.speedModifier = pSpeedModifier;
             this.setFlags(EnumSet.of(Goal.Flag.MOVE));
@@ -300,13 +360,14 @@ public class Rascal extends PathfinderMob implements IAnimatable {
         public boolean canUse() {
             LivingEntity target = this.mob.getTarget();
 
-            if (target != null) {
-                if (target.distanceToSqr(this.mob) < 256.0D) {
-                    return this.setWantedPos();
-                }
+            if (!this.mob.isLookedAt()) {
+                System.out.println("Jaasss");
+                return false;
             }
 
-            return false;
+            System.out.println("OUi Oui");
+            return this.setWantedPos();
+
         }
 
         protected boolean setWantedPos() {
@@ -330,6 +391,11 @@ public class Rascal extends PathfinderMob implements IAnimatable {
 
         }
 
+        public void stop() {
+            this.mob.getNavigation().stop();
+            super.stop();
+        }
+
         @Nullable
         protected Vec3 getHidePos() {
             RandomSource randomsource = this.mob.getRandom();
@@ -337,7 +403,7 @@ public class Rascal extends PathfinderMob implements IAnimatable {
 
             for(int i = 0; i < 10; ++i) {
                 BlockPos blockpos1 = blockpos.offset(randomsource.nextInt(20) - 10, randomsource.nextInt(6) - 3, randomsource.nextInt(20) - 10);
-                if (/*!(this.mob.getLevel().getBrightness(LightLayer.BLOCK, this.mob.blockPosition()) == 0)*/ !this.mob.level.canSeeSky(blockpos1) && this.mob.getWalkTargetValue(blockpos1) < 0.0F) {
+                if (!(this.mob.getLevel().getBrightness(LightLayer.BLOCK, this.mob.blockPosition()) == 0) && (this.mob.getWalkTargetValue(blockpos1) < 10.0F)) {
                     return Vec3.atBottomCenterOf(blockpos1);
                 }
             }
@@ -347,8 +413,9 @@ public class Rascal extends PathfinderMob implements IAnimatable {
 
         @Override
         public void tick() {
-            super.tick();
             System.out.println("ah yes");
+            super.tick();
+
         }
     }
 }
