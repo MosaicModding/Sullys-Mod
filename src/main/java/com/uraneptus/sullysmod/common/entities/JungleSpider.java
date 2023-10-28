0bbaecd61b3d18015d38379bdc4657a6f7b6ce3e
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2023-10-28 10:08:17 +0100

    Jungle Spider Part 2

--- src/main/java/com/uraneptus/sullysmod/common/entities/JungleSpider.java
+++ src/main/java/com/uraneptus/sullysmod/common/entities/JungleSpider.java
@@ -27,6 +27,7 @@ import net.minecraft.world.entity.ai.navigation.WallClimberNavigation;
 import net.minecraft.world.entity.animal.IronGolem;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Skeleton;
+import net.minecraft.world.entity.monster.Spider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
@@ -39,15 +40,14 @@ import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class JungleSpider extends Monster {
+public class JungleSpider extends Spider {
     private static final EntityDataAccessor<Byte> DATA_FLAGS_ID = SynchedEntityData.defineId(JungleSpider.class, EntityDataSerializers.BYTE);
-    private final List<MobEffect> BENEFICIAL_MOB_EFFECTS = new ArrayList<>();
-    private final List<MobEffect> HARMFUL_MOB_EFFECTS = new ArrayList<>();
+    private final List<MobEffect> BENEFICIAL_VENOM_EFFECTS = new ArrayList<>();
+    private final List<MobEffect> HARMFUL_VENOM_EFFECTS = new ArrayList<>();
 
     private MobEffect beneficialEffect;
     private MobEffect harmfulEffect;
 
-    private static final float SPIDER_SPECIAL_EFFECT_CHANCE = 0.1F;
 
     public JungleSpider(EntityType<? extends JungleSpider> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
@@ -68,12 +68,6 @@ public class JungleSpider extends Monster {
         this.targetSelector.addGoal(3, new JungleSpider.JungleSpiderTargetGoal<>(this, IronGolem.class));
     }
 
-    /**
-     * Returns the Y offset from the entity's position for any entity riding this one.
-     */
-    public double getPassengersRidingOffset() {
-        return (double)(this.getBbHeight() * 0.5F);
-    }
 
     protected PathNavigation createNavigation(Level pLevel) {
         return new WallClimberNavigation(this, pLevel);
@@ -87,52 +81,37 @@ public class JungleSpider extends Monster {
     /**
      * Called to update the entity's position/logic.
      */
+    @Override
     public void tick() {
         super.tick();
         if (!this.level().isClientSide) {
             this.setClimbing(this.horizontalCollision);
         }
-
-    }
-
-    public static AttributeSupplier.Builder createAttributes() {
-        return Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 16.0D).add(Attributes.MOVEMENT_SPEED, (double)0.3F);
-    }
-
-    protected SoundEvent getAmbientSound() {
-        return SoundEvents.SPIDER_AMBIENT;
-    }
-
-    protected SoundEvent getHurtSound(DamageSource pDamageSource) {
-        return SoundEvents.SPIDER_HURT;
-    }
-
-    protected SoundEvent getDeathSound() {
-        return SoundEvents.SPIDER_DEATH;
-    }
-
-    protected void playStepSound(BlockPos pPos, BlockState pBlock) {
-        this.playSound(SoundEvents.SPIDER_STEP, 0.15F, 1.0F);
-    }
-
-    /**
-     * Returns {@code true} if this entity should move as if it were on a ladder (either because it's actually on a
-     * ladder, or for AI reasons)
-     */
-    public boolean onClimbable() {
-        return this.isClimbing();
-    }
-
-    public void makeStuckInBlock(BlockState pState, Vec3 pMotionMultiplier) {
-        if (!pState.is(Blocks.COBWEB)) {
-            super.makeStuckInBlock(pState, pMotionMultiplier);
+        if (this.harmfulEffect == MobEffects.BLINDNESS && this.beneficialEffect == MobEffects.NIGHT_VISION) {
+            this.harmfulEffect = chooseHarmfulEffect();
+        }
+        if (this.harmfulEffect == MobEffects.DIG_SLOWDOWN && this.beneficialEffect == MobEffects.DIG_SPEED) {
+            this.beneficialEffect = chooseBeneficialEffect();
+        }
+        if (this.harmfulEffect == MobEffects.MOVEMENT_SLOWDOWN && this.beneficialEffect == MobEffects.MOVEMENT_SPEED) {
+            this.beneficialEffect = chooseBeneficialEffect();
+        }
+        if(this.harmfulEffect == MobEffects.UNLUCK && this.beneficialEffect == MobEffects.LUCK) {
+            this.harmfulEffect = chooseHarmfulEffect();
+        }
+        if (this.harmfulEffect == null) {
+            this.harmfulEffect = chooseHarmfulEffect();
+        }
+        if (this.beneficialEffect == null) {
+            this.beneficialEffect = chooseBeneficialEffect();
         }
 
     }
 
-    public MobType getMobType() {
-        return MobType.ARTHROPOD;
+    public static AttributeSupplier.Builder createAttributes() {
+        return Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 18.0D).add(Attributes.MOVEMENT_SPEED, (double)0.3F);
     }
+
 
     public boolean canBeAffected(MobEffectInstance pPotioneffect) {
         if (pPotioneffect.getEffect() == MobEffects.POISON) {
@@ -147,25 +126,8 @@ public class JungleSpider extends Monster {
      * Returns {@code true} if the WatchableObject (Byte) is 0x01 otherwise returns {@code false}. The WatchableObject is
      * updated using setBesideClimbableBlock.
      */
-    public boolean isClimbing() {
-        return (this.entityData.get(DATA_FLAGS_ID) & 1) != 0;
-    }
 
-    /**
-     * Updates the WatchableObject (Byte) created in entityInit(), setting it to 0x01 if par1 is true or 0x00 if it is
-     * false.
-     */
-    public void setClimbing(boolean pClimbing) {
-        byte b0 = this.entityData.get(DATA_FLAGS_ID);
-        if (pClimbing) {
-            b0 = (byte)(b0 | 1);
-        } else {
-            b0 = (byte)(b0 & -2);
-        }
-
-        this.entityData.set(DATA_FLAGS_ID, b0);
-    }
-
+    @Override
     public boolean doHurtTarget(Entity pEntity) {
         if (super.doHurtTarget(pEntity)) {
             if (pEntity instanceof LivingEntity) {
@@ -176,6 +138,7 @@ public class JungleSpider extends Monster {
                     i = 8;
                 }
                 ((LivingEntity)pEntity).addEffect(new MobEffectInstance(this.harmfulEffect, i * 20, 0), this);
+                ((LivingEntity)pEntity).addEffect(new MobEffectInstance(this.beneficialEffect, i * 20, 0), this);
             }
 
             return true;
@@ -187,21 +150,21 @@ public class JungleSpider extends Monster {
     public void createEffectLists() {
         ForgeRegistries.MOB_EFFECTS.iterator().forEachRemaining(mobEffect -> {
 
-            if (mobEffect.getCategory() == MobEffectCategory.BENEFICIAL && ForgeRegistries.MOB_EFFECTS.getHolder(ForgeRegistries.MOB_EFFECTS.getKey(mobEffect)).orElseThrow().is(SMMobEffectTags.JUNGLE_SPIDER_BENEFICIAL_EFFECTS)) {
-                BENEFICIAL_MOB_EFFECTS.add(mobEffect);
+            if ((mobEffect.getCategory() == MobEffectCategory.BENEFICIAL || mobEffect.getCategory() == MobEffectCategory.NEUTRAL) && ForgeRegistries.MOB_EFFECTS.getHolder(ForgeRegistries.MOB_EFFECTS.getKey(mobEffect)).orElseThrow().is(SMMobEffectTags.JUNGLE_SPIDER_BENEFICIAL_OR_NEUTRAL_VENOM_EFFECTS)) {
+                BENEFICIAL_VENOM_EFFECTS.add(mobEffect);
             }
-            if (mobEffect.getCategory() == MobEffectCategory.HARMFUL && ForgeRegistries.MOB_EFFECTS.getHolder(ForgeRegistries.MOB_EFFECTS.getKey(mobEffect)).orElseThrow().is(SMMobEffectTags.JUNGLE_SPIDER_HARMFUL_EFFECTS)) {
-                HARMFUL_MOB_EFFECTS.add(mobEffect);
+            if (mobEffect.getCategory() == MobEffectCategory.HARMFUL && ForgeRegistries.MOB_EFFECTS.getHolder(ForgeRegistries.MOB_EFFECTS.getKey(mobEffect)).orElseThrow().is(SMMobEffectTags.JUNGLE_SPIDER_HARMFUL_VENOM_EFFECTS)) {
+                HARMFUL_VENOM_EFFECTS.add(mobEffect);
             }
         });
     }
 
     private MobEffect chooseBeneficialEffect() {
-        return BENEFICIAL_MOB_EFFECTS.get(random.nextInt(BENEFICIAL_MOB_EFFECTS.size()));
+        return BENEFICIAL_VENOM_EFFECTS.get(random.nextInt(BENEFICIAL_VENOM_EFFECTS.size()));
     }
 
     private MobEffect chooseHarmfulEffect() {
-        return HARMFUL_MOB_EFFECTS.get(this.level().random.nextInt(HARMFUL_MOB_EFFECTS.size()));
+        return HARMFUL_VENOM_EFFECTS.get(random.nextInt(HARMFUL_VENOM_EFFECTS.size()));
     }
     @Override
     public void addAdditionalSaveData(CompoundTag compoundTag) {
@@ -221,70 +184,6 @@ public class JungleSpider extends Monster {
     public SpawnGroupData finalizeSpawn(ServerLevelAccessor pLevel, DifficultyInstance pDifficulty, MobSpawnType pReason, @Nullable SpawnGroupData pSpawnData, @Nullable CompoundTag pDataTag) {
         pSpawnData = super.finalizeSpawn(pLevel, pDifficulty, pReason, pSpawnData, pDataTag);
         RandomSource randomsource = pLevel.getRandom();
-
-
-
-
-        //this.entityData.define(NEGATIVE_VENOM_EFFECT_NUMBER, random.nextInt(SullysMod.HARMFUL_MOB_EFFECTS.size()));
-
-        /*switch (chance) {
-            case 0:
-                this.entityData.define(NEGATIVE_VENOM_EFFECT, "poison");
-                break;
-            case 1:
-                this.entityData.define(NEGATIVE_VENOM_EFFECT, "blindness");
-                break;
-            case 2:
-                this.entityData.define(NEGATIVE_VENOM_EFFECT, "slowness");
-                break;
-            case 3:
-                this.entityData.define(NEGATIVE_VENOM_EFFECT, "hunger");
-                break;
-            case 4:
-                this.entityData.define(NEGATIVE_VENOM_EFFECT, "weakness");
-                break;
-            case 5:
-                this.entityData.define(NEGATIVE_VENOM_EFFECT, "darkness");
-                break;
-            case 6:
-                this.entityData.define(NEGATIVE_VENOM_EFFECT, "nausea");
-                break;
-        }
-        int chance2 = random.nextInt(6);
-        switch (chance2) {
-            case 0:
-                if (!this.entityData.get(NEGATIVE_VENOM_EFFECT).equals("slowness")) {
-                    this.entityData.define(POSITIVE_VENOM_EFFECT, "speed");
-                }
-                this.entityData.define(POSITIVE_VENOM_EFFECT, "water_breathing");
-                break;
-            case 1:
-                this.entityData.define(POSITIVE_VENOM_EFFECT, "jump_boost");
-                break;
-            case 2:
-                this.entityData.define(POSITIVE_VENOM_EFFECT, "glowing");
-                break;
-            case 3:
-                this.entityData.define(POSITIVE_VENOM_EFFECT, "slow_falling");
-                break;
-            case 4:
-                if (!this.entityData.get(NEGATIVE_VENOM_EFFECT).equals("hunger")) {
-                    this.entityData.define(POSITIVE_VENOM_EFFECT, "saturation");
-                }
-                this.entityData.define(POSITIVE_VENOM_EFFECT, "fire_resistance");
-                break;
-            case 5:
-                if (!this.entityData.get(NEGATIVE_VENOM_EFFECT).equals("darkness") && !this.entityData.get(NEGATIVE_VENOM_EFFECT).equals("blindness")) {
-                    this.entityData.define(POSITIVE_VENOM_EFFECT, "night_vision");
-                }
-                this.entityData.define(POSITIVE_VENOM_EFFECT, "luck");
-                break;
-        }
-
-
-         */
-
-
 
 
         if (randomsource.nextInt(100) == 0) {
@@ -313,8 +212,9 @@ public class JungleSpider extends Monster {
         return pSpawnData;
     }
 
+    @Override
     protected float getStandingEyeHeight(Pose pPose, EntityDimensions pSize) {
-        return 0.65F;
+        return 0.45F;
     }
 
     static class JungleSpiderAttackGoal extends MeleeAttackGoal {
--- src/main/java/com/uraneptus/sullysmod/core/registry/SMEntityTypes.java
+++ src/main/java/com/uraneptus/sullysmod/core/registry/SMEntityTypes.java
@@ -17,5 +17,5 @@ public class SMEntityTypes {
     public static final RegistryObject<EntityType<Tortoise>> TORTOISE = HELPER.createLivingEntity("tortoise", Tortoise::new, MobCategory.CREATURE, 1.0F, 0.9F);
     public static final RegistryObject<EntityType<TortoiseShell>> TORTOISE_SHELL = HELPER.createEntity("tortoise_shell", TortoiseShell::new, TortoiseShell::new, MobCategory.MISC, 1.1F, 0.9F);
     public static final RegistryObject<EntityType<BoulderingZombie>> BOULDERING_ZOMBIE = HELPER.createLivingEntity("bouldering_zombie", BoulderingZombie::new, MobCategory.MONSTER, 0.6F, 1.95F);
-    public static final RegistryObject<EntityType<JungleSpider>> JUNGLE_SPIDER = HELPER.createLivingEntity("jungle_spider", JungleSpider::new, MobCategory.MONSTER, 1.4F, 0.9F);
+    public static final RegistryObject<EntityType<JungleSpider>> JUNGLE_SPIDER = HELPER.createLivingEntity("jungle_spider", JungleSpider::new, MobCategory.MONSTER, 0.85F, 0.9F);
 }
