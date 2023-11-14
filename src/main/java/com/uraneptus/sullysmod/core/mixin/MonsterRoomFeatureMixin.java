ca1409846c148d69e1b05bc4711b0166619e81b1
Mana Kampman <manastar04@gmail.com>
2023-11-14 05:32:40 -0800

    Add Bouldering Zombie spawners

---
+++ src/main/java/com/uraneptus/sullysmod/core/mixin/MonsterRoomFeatureMixin.java
@@ -0,0 +1,33 @@
+package com.uraneptus.sullysmod.core.mixin;
+
+import com.uraneptus.sullysmod.core.SMConfig;
+import com.uraneptus.sullysmod.core.registry.SMEntityTypes;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.MonsterRoomFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+
+@Mixin(MonsterRoomFeature.class)
+public class MonsterRoomFeatureMixin {
+
+    @Inject(method = "place", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/SpawnerBlockEntity;setEntityId(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/util/RandomSource;)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+
+    private void replaceZombie (FeaturePlaceContext<NoneFeatureConfiguration> pContext, CallbackInfoReturnable<Boolean> cir, Predicate predicate, BlockPos blockpos, RandomSource randomsource, WorldGenLevel worldgenlevel, int i, int j, int k, int l, int i1, int j1, int k1, int l1, int i2, int j2, BlockEntity blockentity, SpawnerBlockEntity spawnerblockentity) {
+        if (Objects.requireNonNull(spawnerblockentity.getSpawner().getOrCreateDisplayEntity(Objects.requireNonNull(spawnerblockentity.getLevel()), randomsource, blockpos)).getType() == EntityType.ZOMBIE && SMConfig.DISABLE_DEEPSLATE_ZOMBIE_SPAWNS.get() && blockpos.getY() <= 0) {
+            spawnerblockentity.setEntityId(SMEntityTypes.BOULDERING_ZOMBIE.get(), randomsource);
+        }
+    }
+}
