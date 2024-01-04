f79f7bde39229bd4a02919b188ab544215332819
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-01-04 08:55:33 +0000

    Removed Jade Compat

---
+++ src/main/java/com/uraneptus/sullysmod/common/blocks/AmberBlock.java
@@ -0,0 +1,9 @@
+package com.uraneptus.sullysmod.common.blocks;
+
+import net.minecraft.world.level.block.Block;
+
+public class AmberBlock extends Block {
+    public AmberBlock(Properties pProperties) {
+        super(pProperties);
+    }
+}
