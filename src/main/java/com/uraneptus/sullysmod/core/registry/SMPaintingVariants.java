d60054b44bd1c5b0b902c139a05ab4900cc5564e
Uraneptus <uraneptus33@gmail.com>
2024-09-24 00:24:02 +0200

    updated paintings & ship cosmetics by default

--- src/main/java/com/uraneptus/sullysmod/core/registry/SMPaintingVariants.java
+++ src/main/java/com/uraneptus/sullysmod/core/registry/SMPaintingVariants.java
@@ -13,7 +13,7 @@ public class SMPaintingVariants {
     public static final DeferredRegister<PaintingVariant> PAINTINGS = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, SullysMod.MOD_ID);
     public static Map<String, String> PAINTING_TRANSLATIONS = new HashMap<>();
 
-    public static final RegistryObject<PaintingVariant> UNNERVING_NIGHT = registerPainting("unnerving_night", "RealSpidey", 16, 32);
+    public static final RegistryObject<PaintingVariant> UNNERVING_NIGHT = registerPainting("unnerving_night", "RealSpidey", 16, 48);
     public static final RegistryObject<PaintingVariant> AMBER = registerPainting("amber", "Graus", 16, 32);
     public static final RegistryObject<PaintingVariant> INFESTATION = registerPainting("infestation", "Ninni", 32, 32);
     public static final RegistryObject<PaintingVariant> LAKE = registerPainting("lake", "Farcr", 16, 48);
@@ -21,6 +21,11 @@ public class SMPaintingVariants {
     public static final RegistryObject<PaintingVariant> RIVER_TERROR = registerPainting("river_terror", "Graus", 32, 32);
     public static final RegistryObject<PaintingVariant> MESMURRIZING = registerPainting("mesmurrizing", "Graus", 32, 32);
     public static final RegistryObject<PaintingVariant> JADE_DRAGON = registerPainting("jade_dragon", "Shable", 64, 64);
+    public static final RegistryObject<PaintingVariant> A_VISITOR = registerPainting("a_visitor", "Ibrokemyribcage", 64, 32);
+    public static final RegistryObject<PaintingVariant> BEGINNING = registerPainting("beginning", "Sully", 16, 48);
+    public static final RegistryObject<PaintingVariant> HOME = registerPainting("home", "Ibrokemyribcage", 64, 64);
+    public static final RegistryObject<PaintingVariant> ILLAGER_BEAST = registerPainting("illager_beast", "Angery", 64, 48);
+    public static final RegistryObject<PaintingVariant> THANK_YOU = registerPainting("thank_you", "Sully", 64, 64);
 
     public static RegistryObject<PaintingVariant> registerPainting(String name, String author, int width, int height) {
         PAINTING_TRANSLATIONS.put(name, author);
