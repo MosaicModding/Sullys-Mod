643972bbd62b3b9db3a5fbd80c92c9724c01e5c7
Uraneptus <uraneptus33@gmail.com>
2025-07-03 00:45:15 +0200

    closes #137

--- src/main/java/com/uraneptus/sullysmod/common/entities/Tortoise.java
+++ src/main/java/com/uraneptus/sullysmod/common/entities/Tortoise.java
@@ -325,8 +325,8 @@ public class Tortoise extends Animal implements WorkstationAttachable {
         if (this.getHideTimerDuration() < durationInTicks || durationInTicks == this.getHideTimerDuration() - 1) {
             if (this.getHideTimerDuration() == 0) {
                 level.playSound(null, this.blockPosition(), SMSounds.TORTOISE_HIDE.get(), SoundSource.AMBIENT, 1.0F, 1.0F);
+                this.dropLeash(true, true);
             }
-            this.dropLeash(true, true);
             this.entityData.set(HIDE_TIMER, durationInTicks);
             this.refreshDimensions();
         }
